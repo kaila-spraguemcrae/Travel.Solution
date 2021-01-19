5822ff37e505728ed2d39ea828d1cb217e59e3f5
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-01-19 12:18:23 -0800

    add models and context

---
+++ Travel/Models/TravelContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Travel.Models
+{
+    public class TravelContext : DbContext
+    {
+        public TravelContext(DbContextOptions<TravelContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Destination> Destinations { get; set; }
+
+    }
+}
